adf8ba8e4e49782f04a35089c5b609b85ea232a0
Joohwan Oh <joohwan.oh@outlook.com>
2016-08-19 20:19:07 -0400

    Include coverage with Coveralls in .travis.yml

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(
     url='https://github.com/joowani/python-arango',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['requests', 'pytest', 'six']
+    install_requires=['requests', 'six']
 )
