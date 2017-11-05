1dff440ddbb6086545267b4b2cdbc40cdc1b2511
dancollins34 <dancollins34@gmail.com>
2017-11-04 20:27:15 -0400

    Incorrect direction for versioned import

--- arango/http_clients/__init__.py
+++ arango/http_clients/__init__.py
@@ -2,5 +2,5 @@ import sys
 
 from arango.http_clients.default import DefaultHTTPClient
 
-if sys.version_info < (3,5):
+if sys.version_info >= (3, 5):
     from arango.http_clients.aio import AsyncioHTTPClient
