00ec73efa28f24d5a5dae8187564a78761971b3a
aMahanna <anthony.mahanna@gmail.com>
2021-12-03 20:36:00 -0500

    cleanup: RandomHostResolver

--- arango/resolver.py
+++ arango/resolver.py
@@ -32,19 +32,20 @@ class RandomHostResolver(HostResolver):
     """Random host resolver."""
 
     def __init__(self, host_count: int) -> None:
+        self._max = host_count - 1
         self.max_tries = host_count * 3
-        self._count = host_count
         self._prev_host_indexes: Set[int] = set()
 
     def get_host_index(self, prev_host_index: Optional[int] = None) -> int:
         if prev_host_index is not None:
-            self._prev_host_indexes.add(prev_host_index)
-            if len(self._prev_host_indexes) == self._count:
+            if len(self._prev_host_indexes) == self._max:
                 self._prev_host_indexes.clear()
+
+            self._prev_host_indexes.add(prev_host_index)
 
         host_index = None
         while host_index is None or host_index in self._prev_host_indexes:
-            host_index = random.randint(0, self._count - 1)
+            host_index = random.randint(0, self._max)
 
         return host_index
 
