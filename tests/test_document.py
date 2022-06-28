7eaa046b54c8304bddfd7d5e080e64f3a07514ee
aMahanna <anthony.mahanna@gmail.com>
2022-06-27 22:25:42 -0400

    fix: black

--- tests/test_document.py
+++ tests/test_document.py
@@ -1842,7 +1842,7 @@ def test_document_import_bulk(col, bad_col, docs):
     assert type(result) is list
     assert len(result) == 1
     empty_collection(col)
-    
+
     # Test import bulk with overwrite and batch_size
     with pytest.raises(ValueError):
         col.import_bulk(docs, overwrite=True, batch_size=1)
