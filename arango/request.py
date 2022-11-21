0de7d511f77a23a0191772072fa507d1ba51df2e
tjoubert <terence.joubert@outlook.com>
2022-11-21 11:17:02 +0400

    Re-sorted imports

--- arango/request.py
+++ arango/request.py
@@ -2,7 +2,7 @@ __all__ = ["Request"]
 
 from typing import Any, MutableMapping, Optional
 
-from arango.typings import Fields, Headers, Params, DriverFlags
+from arango.typings import DriverFlags, Fields, Headers, Params
 
 
 def normalize_headers(
