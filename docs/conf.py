68f6f205f353c2a91a77275aebc20502fdece454
Anthony Mahanna <anthony.mahanna@gmail.com>
2023-08-15 16:38:24 -0400

    update sys path

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(1, os.path.dirname(os.path.abspath("../")) + os.sep + "arango")
 
 project = "python-arango"
 copyright = "2016-2022, Joohwan Oh"
