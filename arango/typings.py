9776c1e62f010a3ca1a3073bd71511194efeb1aa
Valentin Grégoire <32612304+valentingregoire@users.noreply.github.com>
2023-09-20 16:09:16 +0200

    fix: Added all allowed data types to the bind_vars. (#289)
    Co-authored-by: ValentG2 <valentin.gregoire@basf.com>

--- arango/typings.py
+++ arango/typings.py
@@ -1,6 +1,16 @@
-__all__ = ["Fields", "Headers", "Json", "Jsons", "Params"]
+__all__ = [
+    "Fields",
+    "Headers",
+    "Json",
+    "Jsons",
+    "Params",
+    "PrimitiveDataTypes",
+    "CompoundDataTypes",
+    "DataTypes",
+]
 
-from typing import Any, Dict, List, MutableMapping, Sequence, Union
+from numbers import Number
+from typing import Any, Dict, List, MutableMapping, Optional, Sequence, Union
 
 Json = Dict[str, Any]
 Jsons = List[Json]
@@ -8,3 +18,11 @@ Params = MutableMapping[str, Union[bool, int, str]]
 Headers = MutableMapping[str, str]
 Fields = Union[str, Sequence[str]]
 DriverFlags = List[str]
+PrimitiveDataTypes = Optional[Union[bool, Number, str]]
+CompoundDataTypes = Optional[
+    Union[
+        Sequence[Optional[Union[PrimitiveDataTypes, "CompoundDataTypes"]]],
+        MutableMapping[str, Optional[Union[PrimitiveDataTypes, "CompoundDataTypes"]]],
+    ]
+]
+DataTypes = Optional[Union[PrimitiveDataTypes, CompoundDataTypes]]
