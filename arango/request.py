c6a86ce3de44d600ae4b360de7b8b86105c884ad
Alex Petenchea <alex.petenchea@gmail.com>
2024-07-21 19:24:44 +0300

    Keeping version at 8.1.0

--- arango/request.py
+++ arango/request.py
@@ -12,7 +12,7 @@ def normalize_headers(
     if driver_flags is not None:
         for flag in driver_flags:
             flags = flags + flag + ";"
-    driver_version = "9.0.0"
+    driver_version = "8.1.0"
     driver_header = "python-arango/" + driver_version + " (" + flags + ")"
     normalized_headers: Headers = {
         "charset": "utf-8",
