113c116947219a3c55efaa577d1831f061c15568
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-11-13 22:11:38 -0500

    new: add `_hosts` to `BaseConnection`

--- arango/connection.py
+++ arango/connection.py
@@ -47,6 +47,7 @@ class BaseConnection:
         request_compression: Optional[RequestCompression] = None,
         response_compression: Optional[str] = None,
     ) -> None:
+        self._hosts = hosts
         self._url_prefixes = [f"{host}/_db/{db_name}" for host in hosts]
         self._host_resolver = host_resolver
         self._sessions = sessions
